d33afc9b23d5aa65e6016201bbedbf3718677604
andrey <andrey.kozhevnikov@devexpress.com>
2022-12-14 17:49:49 +0400

    use MyEFSolutionEFCoreDbContextNew in  MyEFSolutionContextInitializer and MyEFSolutionDesignTimeDbContextFactory

--- CS/MyEFSolution/MyEFSolution.Module/BusinessObjects/MyEFSolutionDbContext.cs
+++ CS/MyEFSolution/MyEFSolution.Module/BusinessObjects/MyEFSolutionDbContext.cs
@@ -5,6 +5,7 @@ using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
 using DevExpress.Persistent.BaseImpl.EF;
 using DevExpress.ExpressApp.Design;
 using DevExpress.ExpressApp.EFCore.DesignTime;
+using MyEFSolution.Module.MyModels;
 
 namespace MyEFSolution.Module.BusinessObjects;
 
@@ -12,16 +13,16 @@ namespace MyEFSolution.Module.BusinessObjects;
 // For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
 public class MyEFSolutionContextInitializer : DbContextTypesInfoInitializerBase {
 	protected override DbContext CreateDbContext() {
-		var optionsBuilder = new DbContextOptionsBuilder<MyEFSolutionEFCoreDbContext>()
+		var optionsBuilder = new DbContextOptionsBuilder<MyEFSolutionEFCoreDbContextNew>()
             .UseSqlServer(";")
             .UseChangeTrackingProxies()
             .UseObjectSpaceLinkProxies();
-        return new MyEFSolutionEFCoreDbContext(optionsBuilder.Options);
+        return new MyEFSolutionEFCoreDbContextNew(optionsBuilder.Options);
 	}
 }
 //This factory creates DbContext for design-time services. For example, it is required for database migration.
-public class MyEFSolutionDesignTimeDbContextFactory : IDesignTimeDbContextFactory<MyEFSolutionEFCoreDbContext> {
-	public MyEFSolutionEFCoreDbContext CreateDbContext(string[] args) {
+public class MyEFSolutionDesignTimeDbContextFactory : IDesignTimeDbContextFactory<MyEFSolutionEFCoreDbContextNew> {
+	public MyEFSolutionEFCoreDbContextNew CreateDbContext(string[] args) {
 		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
 		//var optionsBuilder = new DbContextOptionsBuilder<MyEFSolutionEFCoreDbContext>();
 		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=MyEFSolution");
